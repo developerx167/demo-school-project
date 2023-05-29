7f64a64c38ae0b9f5c294b3a2a32ba0de03200e8
devx167 <developerx167@gmail.com>
2023-05-29 15:25:22 +0530

    feat: use env instead of hardcode mongo uri

--- lib/mongoConn.ts
+++ lib/mongoConn.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export default async function mongoConn ()
 {
-    return await mongoose.connect(`mongodb+srv://developerx167:7lLEJU6DxLX4KPh1@excelholic.6vt4fpq.mongodb.net/test?retryWrites=true&w=majority`);
+    return await mongoose.connect(process.env.MONGODB_URI!);
 }
